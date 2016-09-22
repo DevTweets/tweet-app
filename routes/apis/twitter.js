8148507bf2f4445088336b8cd42fa3aa08f854f7
hikurangi <hikurangi@gmail.com>
2016-09-22 17:23:35 +1200

    environment variables;

--- routes/apis/twitter.js
+++ routes/apis/twitter.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const Twit = require('twit')
 const T = new Twit({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY || TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET || TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY || TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET || TWITTER_ACCESS_TOKEN_SECRET
 })
 
 console.log(process.env);
