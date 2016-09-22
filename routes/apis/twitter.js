f39de0b7ae20c90ee011c8d9c79d663f7661d4e6
bradymat <bradymat@gmail.com>
2016-09-22 16:42:54 +1200

    api searching to console

---
+++ routes/apis/twitter.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const Twit = require('twit')
+const T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
+
+console.log(process.env);
+const router = require('express').Router()
+const _ = require('lodash')
+
+T.get('search/tweets', { q: 'donkey', count: 10 }, function(err, data, response) {
+  console.log(data)
+})
+
+
+
+module.exports = router
