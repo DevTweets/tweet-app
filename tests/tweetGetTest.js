8b9823a3d5a1fe4d06d675f375d7404fe76cffc8
jock-dalby <jock-dalby@jock-dalby.local>
2016-09-22 12:02:49 +1200

    tape-test update

--- tests/tweetGetTest.js
+++ tests/tweetGetTest.js
@@ -1,16 +1,15 @@
 const test = require('tape')
-// const testData = require('./test-data')
-
-const testUrl = 'https://api.twitter.com/1.1/search/tweets.json?q=%23freebandnames&since_id=24012619984051000&max_id=250126199840518145&result_type=mixed&count=4'
+const testData = require('./test-data')
+//
+// url needed to give testData result 'https://api.twitter.com/1.1/search/tweets.json?q=%23freebandnames&since_id=24012619984051000&max_id=250126199840518145&result_type=mixed&count=4'
 
 test('test setup working', function (t) {
-  t.pass("test working")
+  t.pass()
   t.end()
 })
 
-// test('We got tweets', getTweets(testUrl){
-//   if ('getTweetsReturn' === testData){
-//   t.pass()
-//   }
-//   t.end()
-// })
+test('We got tweets', function(t) {
+  var expected = {}
+  t.equal(getTweets(), testData, "Winner winner" )
+  t.end()
+})
