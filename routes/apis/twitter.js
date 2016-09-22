54742ad0ebd12cc6ebb6f98a687b4fd1984d51a5
hikurangi <hikurangi@gmail.com>
2016-09-23 07:43:05 +1200

    testing 1-2

--- routes/apis/twitter.js
+++ routes/apis/twitter.js
@@ -1,20 +1,55 @@
 require('dotenv').config()
+const router = require('express').Router()
+const _ = require('lodash')
 const Twit = require('twit')
 const T = new Twit({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+
 })
 
-console.log(process.env);
-const router = require('express').Router()
-const _ = require('lodash')
+// const keys = {
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+// }
 
-T.get('search/tweets', { q: 'donkey', count: 10 }, function(err, data, response) {
-  console.log(data)
-})
+// class twitter {
+//   getTweets(endpoint){
+//     var deferred = Promise.defer()
+//
+//     fetch(endpoint, keys, { q: 'donkey', count: 10})
+//       .then((data) => {
+//           deferred.resolve(data)
+//       })
+//       return deferred.promise
+//     }
+//   }
+module.exports = {
+    get: function(keyword) {
+      T.get('search/tweets', { q: keyword, count: 10 }, function(err, data, response) {
+        return data
+    })
+  }
+}
 
+// class twitter {
+//   getTweets(){
+//     var deferred = Promise.defer()
+//     T.get('search/tweets', { q: 'donkey', count: 10 })
+//       .catch(function (err) {
+//       console.log('caught error', err.stack)
+//       })
+//       .then(function (result){
+//       return deferred.promise
+//       console.log('data', result.data);
+//     })
+//     }
+// }
 
 
 module.exports = router
+// module.exports = new twitter
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var twitter = require('./apis/twitter')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tweet App: Avalanche' });
+  res.render();
+  // res.render(getTweets())
 });
 
 router.get('/tweets/all', function(req, res, next) {
-  res.render('tweets', { allTweets: 'Tweets Here' });
+  console.log("Hello")
+  res.render('tweets', { title: 'Tweet App: Avalanche' });
+  // res.render(getTweets())
 });
+
+// var loadTweets = function(endpoint){
+//    twitter.getTweets()
+//     .then((data) =>{
+//       res.render('index', { body: data })
+//     })
+//     .catch ((error) => {
+//       console.log(error.message);
+//     })
+// }
+
+// loadTweets('search/tweets')
+
+//Get Tweets
+
 
 module.exports = router;
