9f232d425ca3fb50e26f01c3f4ffb37d24c6f960
hikurangi <hikurangi@gmail.com>
2016-09-23 14:36:10 +1200

    apis taken out of routes

--- apis/sentiment.js
+++ apis/sentiment.js
@@ -2,3 +2,12 @@ require('dotenv').config()
 const router = require('express').Router()
 const _ = require('lodash')
 const xMashapeKey = process.env.X_MASHAPE_KEY
+
+unirest.post("https://community-sentiment.p.mashape.com/text/")
+.header("X-Mashape-Key", "UseI86IVI3mshnTV8ZFaplX4cPtHp1BjrdAjsnfrLdvCCD5VEI")
+.header("Content-Type", "application/x-www-form-urlencoded")
+.header("Accept", "application/json")
+.send("txt=Today is  a good day")
+.end(function (result) {
+  console.log(result.status, result.headers, result.body);
+});
