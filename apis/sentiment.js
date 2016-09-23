8d0dfd1ea5e4d7cf5d315f1be354bcfe9bfd3932
hikurangi <hikurangi@gmail.com>
2016-09-23 15:51:26 +1200

    sentiment api intergration

--- apis/sentiment.js
+++ apis/sentiment.js
@@ -1,13 +1,45 @@
 require('dotenv').config()
 const router = require('express').Router()
 const _ = require('lodash')
+var unirest = require('unirest')
+// var getTweets = require('./twitter')
 const xMashapeKey = process.env.X_MASHAPE_KEY
 
-unirest.post("https://community-sentiment.p.mashape.com/text/")
-.header("X-Mashape-Key", "UseI86IVI3mshnTV8ZFaplX4cPtHp1BjrdAjsnfrLdvCCD5VEI")
-.header("Content-Type", "application/x-www-form-urlencoded")
-.header("Accept", "application/json")
-.send("txt=Today is  a good day")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
+module.exports = function(tweet){
+  //return a promise
+  return new Promise( function(resolve, reject){
+    unirest.post("https://community-sentiment.p.mashape.com/text/")
+    .header("X-Mashape-Key", xMashapeKey)
+    .header("Content-Type", "application/x-www-form-urlencoded")
+    .header("Accept", "application/json")
+    .send("txt=" + tweet)
+    .end(function (result) {
+      resolve({text: tweet, sentiment: result.body.result.sentiment})
+    })
+  })
+
+}
+
+
+// var array = ["good day", "bad day", "fantastic day"]
+// var stuff = []
+// for (var i = 0; i < array.length; i++) {
+// var temp = array[i]
+// unirest.post("https://community-sentiment.p.mashape.com/text/")
+// .header("X-Mashape-Key", xMashapeKey)
+// .header("Content-Type", "application/x-www-form-urlencoded")
+// .header("Accept", "application/json")
+// .send("txt=" + array[i])
+// .end(function (result) {
+//   // console.log(result.body.result.sentiment);
+//   stuff.push({text: temp, sentiment:result.body.result.sentiment})
+//   console.log(stuff)
+// });
+// console.log(temp)
+// }
+//
+// console.log(stuff)
+
+// for (var i = 0; i < array.length; i++) {
+//   .send(`txt=${array[i]}`)
+// }
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var getTweets = require('../apis/twitter')
-// twitter API
+var getSentiment = require('../apis/sentiment')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('tweets', { title: 'Avalanche' });
 });
 
+// router.get('/tweets/all', function(req, res, next) {
+//   getTweets(req.query.keyword)
+//     .then(function (result) {
+//       console.log(result.data)
+//       return res.render('tweets', { title: 'Avalanche', tweets: result.data.statuses})
+//     })
+// })
+
 router.get('/tweets/all', function(req, res, next) {
   getTweets(req.query.keyword)
-    .then(function (result) {
-      console.log(result.data)
-      return res.render('tweets', { title: 'Avalanche', tweets: result.data.statuses})
-    })
-});
+  .then(function (tweets) {
+      var promises = tweets.map(function (tweet) { return getSentiment(tweet.text)})
+      return Promise.all(promises)
+  })
+  .then(function(tweetsInfo){
+    return res.render('tweets', { title: 'Avalanche', tweets: tweetsInfo})
+  })
+})
+
+// router.get('tweets/all/sentiment', function(req, res, next){
+//   getTweets(req.query.keyword)
+//   .then(function (tweets) {
+//       var promises = tweets.map(function (tweet) { return getSentiment(tweet.text)})
+//       return Promise.all(promises)
+//   })
+// })
 
 module.exports = router
